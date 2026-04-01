18f8f354b5d40d37843070bd51854a603ab1a0f0
rockkrishna982003-lgtm <rockkrishna982003@gmail.com>
2026-04-01 09:48:19 +0530

    Update API token and clean up code formatting

--- vars.py
+++ vars.py
@@ -4,7 +4,7 @@ from os import environ
 # API Configuration
 API_ID = int(os.environ.get("API_ID", "34422904"))
 API_HASH = os.environ.get("API_HASH", "7e0002469784f47fc08a6b3d93d7ebed")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8722180281:AAEFG5aVXBnc-4fEwXq1rnxxVPX9pKEc9Gs")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8335828145:AAGiyUvlqVmuu5JayYoIenBEEmG7I_MVhmU")
 
 CREDIT = os.environ.get("CREDIT", "𝐈𝐓'𝐬𝐆𝐎𝐋𝐔")
 # MongoDB Configuration
